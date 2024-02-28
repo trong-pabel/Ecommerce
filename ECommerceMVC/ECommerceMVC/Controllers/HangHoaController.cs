ded4a80b0e58e0883ec8e993845b0dddf3b7043c
Hữu Trọng Nguyễn <nguyentrong6655@gmail.com>
2024-02-29 00:29:18 +0700

    create project, connect to database, add layout theme, menu category fuction and view this

---
+++ ECommerceMVC/ECommerceMVC/Controllers/HangHoaController.cs
@@ -0,0 +1,36 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMVC.Controllers
+{
+    public class HangHoaController : Controller
+    {
+		private readonly EcommerceContext db;
+
+		public HangHoaController(EcommerceContext context)
+		{
+			db = context;
+		}
+		public IActionResult Index(int? loai)
+        {
+			var hangHoas = db.HangHoas.AsQueryable();
+
+			if (loai.HasValue)
+			{
+				hangHoas = hangHoas.Where(p => p.MaLoai == loai.Value);
+			}
+
+			var result = hangHoas.Select(p => new GoodsVM
+			{
+				MaHh = p.MaHh,
+				TenHH = p.TenHh,
+				DonGia = p.DonGia ?? 0,
+				Hinh = p.Hinh ?? "",
+				MoTaNgan = p.MoTaDonVi ?? "",
+				TenLoai = p.MaLoaiNavigation.TenLoai
+			});
+			return View(result);
+		}
+    }
+}
---
+++ ECommerceMVC/ECommerceMVC/ViewModels/MenuCategoryVM.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerceMVC.ViewModels
+{
+    public class MenuCategoryVM
+    {
+        public int MaLoai { get; set; }
+        public string TenLoai { get; set; }
+        public int SoLuong { get; set; }
+    }
+}
