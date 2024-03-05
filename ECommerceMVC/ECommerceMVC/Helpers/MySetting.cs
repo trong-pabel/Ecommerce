7aa54154188c4595f07c8ebdd15b6b296dae948e
Hữu Trọng Nguyễn <nguyentrong6655@gmail.com>
2024-03-06 00:18:21 +0700

    add VnPay method

--- ECommerceMVC/ECommerceMVC/Helpers/MySetting.cs
+++ ECommerceMVC/ECommerceMVC/Helpers/MySetting.cs
@@ -6,4 +6,12 @@
 		public const string CLAIM_CUSTOMERID = "CustomerID";
 
 	}
+	public class PaymentType
+	{
+		public static string COD = "COD";
+		public static string Paypal = "Paypal";
+		public static string VNPAY = "VnPay";
+		public static string MOMO = "MoMo";
+		public static string STRIPE = "Stripe";
+	}
 }
